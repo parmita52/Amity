8a03788f10fdf95e5d3671d3c7f276146cbf68a9
Cassandra <cassandra.overney@gmail.com>
2017-02-23 11:07:13 -0800

    I copied the mpa stuff over, but it doesn't work yet! :( The reason it doesn't work is because the program is being overloaded. To fix it, we need to run the Map activity on a separate thread. I will try to do that later.

--- app/src/main/java/com/example/android/amity/MapActivity.java
+++ app/src/main/java/com/example/android/amity/MapActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.amity;
 
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,20 +12,33 @@ import android.view.View;
 public class MapActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Runnable runnable = new Runnable() {
+            public void run() {
+                //System.out.println("just got here");
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+
+                Fragment map = getSupportFragmentManager().findFragmentById(R.id.fragment);
+                map.onCreate(savedInstanceState);
             }
-        });
-    }
+        };
+        Thread mythread = new Thread(runnable);
+        mythread.start();
+
+
+
+}
 
 }
