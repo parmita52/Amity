a8a9cfae0fc3d6aca01cf093ac1af43b00ebd312
Fiona <fiona_v_law@yahoo.com>
2017-03-05 12:45:41 -0800

    Vector + selection of Russia

--- app/src/main/java/com/example/android/amity/HelloGlobeFragment.java
+++ app/src/main/java/com/example/android/amity/HelloGlobeFragment.java
@@ -1,15 +1,25 @@
 package com.example.android.amity;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.mousebird.maply.AttrDictionary;
+import com.mousebird.maply.GlobeController;
 import com.mousebird.maply.GlobeMapFragment;
+import com.mousebird.maply.MapController;
+import com.mousebird.maply.Point2d;
+import com.mousebird.maply.Point3d;
 import com.mousebird.maply.QuadImageTileLayer;
 import com.mousebird.maply.RemoteTileInfo;
 import com.mousebird.maply.RemoteTileSource;
+import com.mousebird.maply.SelectedObject;
 import com.mousebird.maply.SphericalMercatorCoordSystem;
+import com.mousebird.maply.VectorObject;
 
 import java.io.File;
 
@@ -53,6 +63,32 @@ public class HelloGlobeFragment extends GlobeMapFragment {
         // add layer and position
         globeControl.addLayer(baseLayer);
         globeControl.animatePositionGeo(-3.6704803, 40.5023056, 5, 1.0);
+        globeControl.setKeepNorthUp(true);
+        globeControl.setZoomLimits(1,1);
+
+        final String url = "https://s3.amazonaws.com/whirlyglobedocs/tutorialsupport/RUS.geojson";
+        GeoJsonHttpTask task = new GeoJsonHttpTask(globeControl);
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, url);
+        // Set controller to be gesture delegate.
+        // Needed to allow selection.
+        globeControl.gestureDelegate = this;
+    }
+
+
+    @Override
+    public void userDidSelect(GlobeController globeControl, SelectedObject[] selObjs, Point2d loc, Point2d screenLoc) {
+        String msg = "Selected feature count: " + selObjs.length;
+        for (SelectedObject obj : selObjs) {
+            // GeoJSON
+            if (obj.selObj instanceof VectorObject) {
+                VectorObject vectorObject = (VectorObject) obj.selObj;
+                AttrDictionary attributes = vectorObject.getAttributes();
+                String adminName = attributes.getString("ADMIN");
+                msg += "\nVector Object: " + adminName;
+            }
+        }
+
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
     }
 
 }
