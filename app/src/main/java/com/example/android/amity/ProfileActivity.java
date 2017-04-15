7f92e0b4422d648df1fe5b77a1c5925c68559103
parmita <parmita52@gmail.com>
2017-04-15 10:23:02 -0700

    Added logout sound

--- app/src/main/java/com/example/android/amity/ProfileActivity.java
+++ app/src/main/java/com/example/android/amity/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.android.amity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -74,6 +75,8 @@ public class ProfileActivity extends AppCompatActivity
                 logOutIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 logOutIntent.putExtra("methodName", "signOut");
                 startActivity(logOutIntent);
+                MediaPlayer mp = MediaPlayer.create(ProfileActivity.this, R.raw.pling);
+                mp.start();
 
             }
         });
